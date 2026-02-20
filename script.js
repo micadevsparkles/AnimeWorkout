64169dc2e88b0bfed6927f2cf8402b6a7b9a4c96
micadevsparkles <53092907+micadevsparkles@users.noreply.github.com>
2026-02-20 14:17:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw9zGmEEAf0GmHH1fE25M0m_Pckr89iwwYxkGtT8XcjEEemLe-Z5E0P63YqcaLX-L_E/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzp5hSXlsSWfIvEBD3icI_lYNjz6HRlGJBLXipFAVO71M9a8klMxlSneWbLfft1oyM/exec";
 
 let userLogado = null;
 let exerciciosDB = []; // Salva a lista de exercícios para pesquisa
